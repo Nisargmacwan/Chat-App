fae17468552897f84cad515c9059f0cc143e183f
Nisargmacwan <nisargmacwan12@gmail.com>
2025-04-11 17:22:23 +0530

    changed vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // ✅ Correct for Render or any static web host
+  base: './', // ✅ Correct for Render or any static web host
   plugins: [react()],
 })
