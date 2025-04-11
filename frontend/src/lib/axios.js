32e5b01f203a1254a931007ad51e3bc9dc83b253
Nisargmacwan <nisargmacwan12@gmail.com>
2025-04-11 17:40:02 +0530

    fixed axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { axiosInstance } from "./axios";
+import { axiosInstance } from "./lib/axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
